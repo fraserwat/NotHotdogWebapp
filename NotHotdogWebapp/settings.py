8bb3e88004a32c6348080d77602d74682f57973c
Fraser Watt <fsrwatt@gmail.com>
2023-06-06 17:14:39 +0100

    fixing db issue

--- NotHotdogWebapp/settings.py
+++ NotHotdogWebapp/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     #     "ENGINE": "django.db.backends.sqlite3",
     #     "NAME": BASE_DIR / "db.sqlite3",
     # }
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(conn_max_age=600)
 }
 
 # Password validation
