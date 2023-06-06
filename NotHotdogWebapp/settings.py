a6a4970aea2ec0e04ba3180937d715b7d5175393
Fraser Watt <fsrwatt@gmail.com>
2023-06-06 20:16:58 +0100

    SSL

--- NotHotdogWebapp/settings.py
+++ NotHotdogWebapp/settings.py
@@ -81,6 +81,10 @@ WSGI_APPLICATION = "NotHotdogWebapp.wsgi.application"
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
